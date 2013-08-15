11fd73ddf7f5ba04224cc685ba76d1dfe7c79147
Dca <dca35311@gmail.com>
2013-08-15 22:03:35 +0800

    取得access_token

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,7 +1,58 @@
+var FB = require('fb');
 
+FB.options({
+    appId:          '390240691097703',
+    appSecret:      '2b48d0351d04e1a658f3181826f779d5',
+    redirectUri:    'http://localhost:3000/login/callback'
+});
+
+exports.home = function(req, res){
+    res.render('index', {
+        title: 'Express',
+        loginUrl : FB.getLoginUrl({ scope: 'user_about_me' })
+    });
+};
 
 exports.showMe = function(req, res){
-    res.render('user/showMe', {
-        title: '個人資訊',
+    var access_token = req.session.access_token;
+
+    FB.setAccessToken(access_token);
+
+    FB.api('/me', { 'limit' : 100 }, function (result) {
+
+        console.log(result);
+
+        res.render('user/showMe', {
+            title: '個人資訊',
+            data : result
+        });
     });
+};
+
+exports.loginCallback = function(req, res){
+    var code = req.query.code;
+
+    FB.napi('oauth/access_token', {
+        client_id:      FB.options('appId'),
+        client_secret:  FB.options('appSecret'),
+        redirect_uri:   FB.options('redirectUri'),
+        code:           code
+    },
+    function extendAccessToken(err, result) {
+        if(err) throw(err);
+        FB.napi('oauth/access_token', {
+            client_id:          FB.options('appId'),
+            client_secret:      FB.options('appSecret'),
+            grant_type:         'fb_exchange_token',
+            fb_exchange_token:  result.access_token
+        }, function (err, result) {
+            if(err) return next(err);
+
+            req.session.access_token = result.access_token;
+            // req.session.expires
+            res.redirect('/me');
+
+        });
+    });
+
 };
--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,13 @@
 module.exports = function(app){
 
-    var home = function(req, res){
-      res.render('index', { title: 'Express' });
-    };
+
 
     var user = require('./controllers/user');
 
-    app.get('/', home);
+    app.get('/', user.home);
+
     app.get('/me', user.showMe);
+    app.get('/login/callback', user.loginCallback);
+
 
 };
