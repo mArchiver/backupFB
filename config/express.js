e9444525602420a4358f6c48ff32720d1695a2b1
Dca <dca35311@gmail.com>
2013-08-15 19:11:53 +0800

    清除多餘的行

--- config/express.js
+++ config/express.js
@@ -4,13 +4,12 @@ module.exports = function(app, config) {
 
     app.configure(function() {
         app.use(express.compress());
-        console.log(config.root);
 
         app.use(express.static(config.root + '/public'));
         app.set('port', config.port);
         app.set('views', config.root + '/app/views');
         app.set('view engine', 'jade');
-        // app.use(express.favicon(config.root + '/public/img/favicon.ico'));
+
         app.use(express.logger('dev'));
         app.use(express.bodyParser());
         app.use(express.methodOverride());
@@ -18,7 +17,6 @@ module.exports = function(app, config) {
         app.use(express.session({ secret: 'keyboard cat' }));
 
         app.use(app.router);
-
 
     });
 
