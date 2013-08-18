33920f9c3b0cde6a036dd29a34de1da7b92eb5f7
Dca <dca35311@gmail.com>
2013-08-18 13:44:43 +0800

    hotfix callback連結

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,7 +3,7 @@ var FB = require('fb');
 FB.options({
     appId:          '390240691097703',
     appSecret:      '2b48d0351d04e1a658f3181826f779d5',
-    redirectUri:    'http://localhost:3000/login/callback'
+    redirectUri:    'http://fb.dca.tw/login/callback'
 });
 
 exports.home = function(req, res){
