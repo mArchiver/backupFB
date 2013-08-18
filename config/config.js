bdeb8b0c24c52e8fb29726dc58388951a7889475
Dca <dca35311@gmail.com>
2013-08-18 14:24:07 +0800

    new config

--- config/config.js
+++ config/config.js
@@ -1,8 +1,30 @@
-module.exports = {
+var development = {
     root: require('path').normalize(__dirname + '/..'),
     port: 3000,
+    baseUri: 'http://localhost:3000',
 
     app: {
         name: 'Application-name'
     },
 };
+
+var production = {
+
+    baseUri: 'http://fb.dca.tw',
+
+};
+
+
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === 'production') {
+    var config = merge( development, production);
+}
+
+
+function merge(obj1,obj2){
+    for (var attrname in obj2) { obj1[attrname] = obj2[attrname]; }
+    return obj1;
+}
+
+module.exports = config;
