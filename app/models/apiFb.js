c7b75f0cdc48bdd165e1bd1ec9b916015622361e
nczz <nczz@users.noreply.github.com>
2013-08-19 13:19:38 +0800

    隱藏一下個人設定資訊
    FB App ID & Secret key

--- app/models/apiFb.js
+++ app/models/apiFb.js
@@ -2,8 +2,8 @@ var FB = require('fb');
 var config = require('../../config/config.js');
 
 FB.options({
-    appId:          '390240691097703',
-    appSecret:      '2b48d0351d04e1a658f3181826f779d5',
+    appId:          '更換成自己申請的FB APP資訊',
+    appSecret:      '更換成自己申請的FB APP資訊',
     redirectUri:    config.baseUri + '/login/callback',
     scope:          'user_about_me,user_photos,friends_photos'
 });
