6e4881defa3c3b9e344e1a773f775ef651f6bbbf
Dca <dca35311@gmail.com>
2013-08-21 00:49:26 +0800

    沒有session就導回首頁

--- app/routes.js
+++ app/routes.js
@@ -6,10 +6,17 @@ module.exports = function(app){
     var album =require('./controllers/album');
 
     app.get('/', user.home);
-
-    app.get('/me', user.showMe);
     app.get('/login/callback', user.loginCallback);
 
+    app.all('*',function(req, res, next) {
+        if (req.session.access_token) {
+            next();
+        }else{
+            res.redirect('/');
+        }
+    });
+
+    app.get('/me', user.showMe);
     app.get('/user/:id', user.showUser);
 
     app.get('/album/:id', album.showAlbum);
