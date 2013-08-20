dcdcb8ad25db6bec1f5494b42839d40b55530900
Dca <dca35311@gmail.com>
2013-08-21 00:37:56 +0800

    獨立的User Model

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,5 @@
 var FB = require('../models/apiFb');
+var User = require('../models/User');
 
 
 exports.home = function(req, res){
@@ -26,12 +27,11 @@ exports.showMe = function(req, res){
 exports.loginCallback = function(req, res){
     var code = req.query.code;
 
-    getAccessToken( code, function(err, result) {
+    User.getAccessToken( code, function(err, result) {
+
         req.session.access_token = result.access_token;
         res.redirect('/me');
     });
-
-
 };
 
 exports.showUser = function(req, res) {
@@ -49,29 +49,3 @@ exports.showUser = function(req, res) {
         });
     });
 };
-
-function getAccessToken(code, callback) {
-    FB.napi('oauth/access_token', {
-            client_id: FB.options('appId'),
-            client_secret: FB.options('appSecret'),
-            redirect_uri: FB.options('redirectUri'),
-            code: code
-        },
-        function extendAccessToken(err, result) {
-            if (err) {
-                callback(err, null);
-            } else {
-                FB.napi('oauth/access_token', {
-                        client_id: FB.options('appId'),
-                        client_secret: FB.options('appSecret'),
-                        grant_type: 'fb_exchange_token',
-                        fb_exchange_token: result.access_token
-                    },
-                    function(err, result) {
-
-                        callback(err, result);
-                    });
-            }
-        });
-
-}
---
+++ app/models/User.js
@@ -0,0 +1,29 @@
+var FB = require('./apiFb.js');
+
+
+
+exports.getAccessToken = function (code, callback) {
+    FB.napi('oauth/access_token', {
+            client_id       : FB.options('appId'),
+            client_secret   : FB.options('appSecret'),
+            redirect_uri    : FB.options('redirectUri'),
+            code            : code
+        },
+
+        function extendAccessToken(err, result) {
+            if (err) {
+                callback(err, null);
+            } else {
+                FB.napi('oauth/access_token', {
+                        client_id: FB.options('appId'),
+                        client_secret: FB.options('appSecret'),
+                        grant_type: 'fb_exchange_token',
+                        fb_exchange_token: result.access_token
+                    },
+                    function(err, result) {
+
+                        callback(err, result);
+                    });
+            }
+        });
+};
